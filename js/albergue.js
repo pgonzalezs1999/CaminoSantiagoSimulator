726e48b698462a7827dd666d53533d30bb44b6b2
Pablo González Sánchez <pgonzalezs1999@gmail.com>
2022-04-28 12:05:11 +0200

    Añadido "calendario" para reservar albergue

---
+++ js/albergue.js
@@ -0,0 +1,9 @@
+function abrirCalendario()
+{
+    document.querySelector(".calendario").style.display = "block";
+}
+
+function cerrarCalendario()
+{
+    document.querySelector(".calendario").style.display = "none";
+}
