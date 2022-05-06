a6ce8176d84a3b5d3a4acc7018d61a8f75aeca6d
Pablo González Sánchez <pgonzalezs1999@gmail.com>
2022-05-06 11:28:47 +0200

    Añadido cordova

--- Santiago/www/js/albergue.js
+++ Santiago/www/js/albergue.js
@@ -1,3 +1,12 @@
+let fotos =
+[
+    "img/albergue01.jpg",
+    "img/mirador01.jpg",
+    "img/albergue02.jpg",
+    "img/albergue03.jpg",
+    "img/mirador02.jpg"
+]
+
 function abrirCalendario()
 {
     document.querySelector(".calendario").style.display = "block";
