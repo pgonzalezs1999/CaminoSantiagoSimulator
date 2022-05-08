244e92c6ba08765936b529b3dcc6e99cb2be012e
SantiagoBarreiroCampos <72943433+SantiagoBarreiroCampos@users.noreply.github.com>
2022-05-08 19:46:21 +0200

    PrincipalPerfil sin css

--- Santiago/www/js/index.js
+++ Santiago/www/js/index.js
@@ -2,10 +2,12 @@ let unaVez = false;
 if(unaVez == false)
 {
     unaVez = true;
+    /*
     setTimeout(
         function()
         {  
             window.location = "login.html";
         }
     , 2000);
+    */
 }
