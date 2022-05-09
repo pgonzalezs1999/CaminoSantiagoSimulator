f386606916c682eff23c6d420b691ce385298a7e
Pablo González Sánchez <pgonzalezs1999@gmail.com>
2022-05-09 10:44:13 +0200

    Arreglado index.js

--- Santiago/www/js/index.js
+++ Santiago/www/js/index.js
@@ -2,12 +2,10 @@ let unaVez = false;
 if(unaVez == false)
 {
     unaVez = true;
-    /*
     setTimeout(
         function()
         {  
             window.location = "login.html";
         }
     , 2000);
-    */
 }
